4aaf8e2a7e6df8a08554729ac4163cb1c7608429
a-teammate <madoe3@web.de>
2014-07-02 01:00:30 +0200

    Ability to set a tag eg for clans

--- src/fpsgame/server.cpp
+++ src/fpsgame/server.cpp
@@ -227,7 +227,7 @@ namespace server
     struct clientinfo
     {
         int clientnum, ownernum, connectmillis, sessionid, overflow;
-        string name, team, mapvote;
+        string name, tag, team, mapvote;
         int playermodel;
         int modevote;
         int privilege;
@@ -347,7 +347,7 @@ namespace server
 
         void reset()
         {
-            name[0] = team[0] = 0;
+            name[0] = team[0] = tag[0] = 0;
             playermodel = -1;
             privilege = PRIV_NONE;
             connected = local = false;
@@ -1801,6 +1801,7 @@ namespace server
             putint(p, ci->clientnum);
             sendstring(ci->name, p);
             sendstring(ci->team, p);
+			sendstring(ci->tag, p);
             putint(p, ci->playermodel);
         }
     }
@@ -3304,10 +3305,13 @@ namespace server
             case N_SWITCHNAME:
             {
                 QUEUE_MSG;
-                getstring(text, p);
+                getstring(text, p); //name
                 filtertext(ci->name, text, false, MAXNAMELEN);
                 if(!ci->name[0]) copystring(ci->name, "unnamed");
                 QUEUE_STR(ci->name);
+				getstring(text, p); //tag
+				filtertext( ci->tag, text, false, MAXTAGLEN);
+				QUEUE_STR(ci->tag);
                 break;
             }
 
