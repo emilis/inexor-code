07f6878e3515818299a98537670ded77ca2e1d39
superTUX <supertux@cupdev.net>
2014-08-07 22:12:20 +0200

    basic structure fore the new weapon system

---
+++ src/fpsgame/weapons/impact.h
@@ -0,0 +1,9 @@
+#ifndef IMPACT_H_
+#define IMPACT_H_
+
+#include "weapon.h"
+
+namespace tux {
+
+
+} /* namespace tux */
---
+++ src/fpsgame/weapons/shot.h
@@ -0,0 +1,25 @@
+#ifndef SHOT_H_
+#define SHOT_H_
+
+#include "weapon.h"
+
+namespace tux {
+
+struct shot;
+
+struct shot_projectile;
+struct shot_ray;
+
+
+struct shot {
+
+	fpsent *owner;
+
+	float srpead;
+};
+
+struct
+
+} /* namespace tux */
+
+#endif /* SHOT_H_ */
