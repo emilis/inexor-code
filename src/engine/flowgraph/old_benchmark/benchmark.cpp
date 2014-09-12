f67d319a36e8485045002dc56882b089c72c049a
IAmNotHanni <cmdfreak@web.de>
2014-09-12 19:08:55 +0200

    benchmarking now with hierarchy system and groups. No time calculations yet.

---
+++ src/engine/flowgraph/old_benchmark/benchmark.cpp
@@ -0,0 +1,81 @@
+/**
+* Benchmark implementation
+*
+*
+*/
+
+/**
+* Include benchmark library
+*/
+#include "benchmark.h"
+
+/**
+* Include SDL library
+* for SDL_GetTicks
+*/
+#include "SDL_timer.h"
+
+/**
+* SDL_GetTicks is equivalent to timeGetTime() on Windows x86,x64
+* ---
+*/
+
+
+
+/**
+* constructor
+*/
+CBenchmark::CBenchmark()
+{
+}
+
+
+/**
+* destructor
+*/
+CBenchmark::~CBenchmark() {
+}
+
+
+/**
+* Start ticking process
+*/
+void CBenchmark::StartTick(char* name) 
+{
+	// start time measurement
+	m_Time[name].start = SDL_GetTicks();
+	// timer is ticking
+	m_Time[name].ticking = true;
+}
+
+/**
+* Stop ticking process
+*/
+void CBenchmark::StopTick(char* name) 
+{
+	m_Time[name].end = SDL_GetTicks();
+	// timer is NO LONGER ticking
+	m_Time[name].ticking = false;
+}
+
+/**
+* Reset timer
+*/
+void CBenchmark::ResetTick(char* name) 
+{
+	// reset both indices
+	m_Time[name].start = m_Time[name].end = 0;
+	// not ticking!
+	m_Time[name].ticking = false;
+}
+
+/**
+* Get time which passed
+*/
+unsigned long CBenchmark::GetTime(char* name) 
+{
+	/**
+	* Calculate time which passed and return it!
+	*/
+	return m_Time[name].end - m_Time[name].start;
+}
